30558d4467a97ae49bc1f573f7301f18e4300836
Sayantan <sayantannag@users.noreply.github.com>
2024-06-10 08:59:41 +0000

    Conversion of DataTypes

---
+++ 01_js_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Sayantan"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = true
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Sayantan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Sayantan" => false
+
+let someNumber = 22;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
